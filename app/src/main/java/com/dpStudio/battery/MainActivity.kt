53195949eca9c9d2a835aac3b032f7dff4ef8376
danphuong1217 <dannvph@gmail.com>
2020-11-02 21:08:18 +0700

    something

--- app/src/main/java/com/dpStudio/battery/MainActivity.kt
+++ app/src/main/java/com/dpStudio/battery/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.dpStudio.battery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initAll()
+    }
+
+    fun initView() {
+
+    }
+
+    fun initAll() {
+        Contrants.createNotificationChannel(applicationContext)
+        startService(Intent(applicationContext, ServiceBatterymonitor::class.java))
     }
 }
